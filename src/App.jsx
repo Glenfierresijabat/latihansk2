b5a4a94d9d68b141e505d70d307a8f24a9735c27
Glenfierresijabat <pernandosijabat5@gmail.com>
2024-10-14 17:46:39 +0700

    sekarep

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-function App() { 
+import React from 'react'
+
+const autolayouts = () => {
   return (
-	  <>
-	    <h1 className="text-3xl font-bold text-secondary">
-	      Hello world! oke oke
-	    </h1>
-		<h1 className="text-3xl font-poppins text-primary">
-	      Hello world! oke oke
-	    </h1>
-		<h1 className="text-3xl">
-	      Hello world! oke oke
-	    </h1>
-		<p>ini font jugas 121212</p>
-	  </>
+    <div>autolayouts</div>
   )
 }
 
-export default App
+export default autolayouts
---
+++ src/Components/Elements/LabeledInput/Label.jsx
@@ -0,0 +1,7 @@
+const Label = () => {
+    return (
+        <label htmlFor="email" className="block text-sm mb-2">
+         Email Address
+        </label>
+    )
+}
